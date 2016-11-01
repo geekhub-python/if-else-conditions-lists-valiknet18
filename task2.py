df54466245c8e904a204c4ebb1a94be2bb7e54b6
valik-pc <valik.v1per@gmail.com>
2016-11-01 19:32:41 +0200

    Implement second task

---
+++ task2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+print('=========')
+if (number1 == number2 and number2 == number3):
+	print(3)
+elif (number1 == number2 or number2 == number3 or number1 == number3):
+	print(2)
+else:
+	print(0)
